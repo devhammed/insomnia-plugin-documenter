6debf0f69d0cb82a2c5026242618e59a3bc06d8b
Hammed Oyedele <itz.harmid@gmail.com>
2020-10-05 12:23:11 +0100

    refactor: improve error handling logics

--- plugin.js
+++ plugin.js
@@ -13,61 +13,74 @@ module.exports.workspaceActions = [
     icon: 'fa-cloud-upload',
     label: 'Export HTML Documentation...',
     action: async (context, { workspace }) => {
-      let outputPath = await context.app.prompt(
-        'Output Path (e.g ~/Documents/My-Docs)',
-        {
-          submitName: 'Export',
-          defaultValue: await context.store.getItem(CONFIG_KEY)
+      try {
+        let outputPath = await context.app.prompt(
+          'Output Path (e.g ~/Documents/My-Docs)',
+          {
+            submitName: 'Export',
+            defaultValue: await context.store.getItem(CONFIG_KEY)
+          }
+        )
+
+        if (!outputPath.length) {
+          outputPath = path.join(
+            context.app.getPath('desktop'),
+            `${slugify(workspace.name)}-API-Docs`
+          )
+        } else {
+          outputPath = untildify(outputPath)
         }
-      )
 
-      if (!outputPath.length) {
-        outputPath = path.join(
-          context.app.getPath('desktop'),
-          `${slugify(workspace.name)}-API-Docs`
-        )
-      } else {
-        outputPath = untildify(outputPath)
-      }
+        await context.store.setItem(CONFIG_KEY, outputPath)
 
-      await context.store.setItem(CONFIG_KEY, outputPath)
+        try {
+          await mkdirp(outputPath)
+        } catch (e) {
+          await context.app.alert(
+            'Something went wrong!',
+            'Unable to create output folder.'
+          )
 
-      try {
-        await mkdirp(outputPath)
-      } catch (e) {
+          return
+        }
+
+        try {
+          copyDir.sync(PACKAGE_DIST_PATH, outputPath, {
+            utimes: false,
+            mode: false,
+            cover: true
+          })
+        } catch (_) {
+          await context.app.alert(
+            'Something went wrong!',
+            'Unable to copy documenter files to output path.'
+          )
+
+          return
+        }
+
+        try {
+          const data = await context.data.export.insomnia({
+            workspace,
+            format: 'json',
+            includePrivate: false
+          })
+
+          fs.writeFileSync(path.join(outputPath, 'insomnia.json'), data)
+        } catch (_) {
+          await context.app.alert(
+            'Something went wrong!',
+            'Unable to export workspace data.'
+          )
+
+          return
+        }
+
         await context.app.alert(
-          'Something went wrong!',
-          'Unable to create output folder.'
+          'All Done!',
+          `Your documentation has been created at "${outputPath}" and it's ready to be deployed!`
         )
-        return
-      }
-
-      const data = await context.data.export.insomnia({
-        workspace,
-        format: 'json',
-        includePrivate: false
-      })
-
-      try {
-        copyDir.sync(PACKAGE_DIST_PATH, outputPath, {
-          utimes: false,
-          mode: false,
-          cover: true
-        })
-      } catch (_) {
-        await context.app.alert(
-          'Something went wrong!',
-          'Unable to copy documenter files to output path.'
-        )
-        return
-      }
-
-      fs.writeFileSync(path.join(outputPath, 'insomnia.json'), data)
-
-      await context.app.alert(
-        'All Done!',
-        `Your documentation has been created at "${outputPath}" and it's ready to be deployed!`
-      )
+      } catch (_) {}
     }
   }
 ]
