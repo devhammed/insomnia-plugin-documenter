c29970645fc7061e1eb0981de65afc5285d3c657
Hammed Oyedele <itz.harmid@gmail.com>
2021-04-07 09:24:51 +0100

    feat: add support for documentation root path

--- plugin.js
+++ plugin.js
@@ -12,14 +12,23 @@ module.exports.workspaceActions = [
     icon: 'fa-cloud-upload',
     label: 'Export HTML Documentation...',
     action: async (context, { workspace }) => {
+      const rootPathConfigKey = `root_path_${workspace._id}`
       const outputPathConfigKey = `output_path_${workspace._id}`
 
       try {
         let outputPath = await context.app.prompt(
           'Output Path (e.g ~/Documents/My-Docs)',
           {
-            submitName: 'Export',
+            submitName: 'Continue',
             defaultValue: await context.store.getItem(outputPathConfigKey)
+          }
+        )
+
+        let rootPath = await context.app.prompt(
+          'Documentation Root Path (No trailing slash e.g /docs)',
+          {
+            submitName: 'Export',
+            defaultValue: await context.store.getItem(rootPathConfigKey)
           }
         )
 
@@ -31,6 +40,12 @@ module.exports.workspaceActions = [
         } else {
           outputPath = untildify(outputPath)
         }
+
+        if (rootPath === '/') {
+          rootPath = ''
+        }
+
+        await context.store.setItem(rootPathConfigKey, rootPath)
 
         await context.store.setItem(outputPathConfigKey, outputPath)
 
@@ -51,6 +66,24 @@ module.exports.workspaceActions = [
             mode: false,
             cover: true
           })
+
+          const indexFilePath = path.join(outputPath, 'index.html')
+
+          fs.writeFileSync(
+            indexFilePath,
+            fs
+              .readFileSync(indexFilePath)
+              .toString()
+              .replace('src="bundle.js"', `src="${rootPath}/bundle.js"`)
+              .replace('href="bundle.css"', `href="${rootPath}/bundle.css"`)
+              .replace('href="favicon.ico"', `href="${rootPath}/favicon.ico"`)
+              .replace(
+                'id="app"',
+                `id="app"${
+                  rootPath.length ? ' data-root="' + rootPath + '"' : ''
+                }`
+              )
+          )
         } catch (_) {
           await context.app.alert(
             'Something went wrong!',
